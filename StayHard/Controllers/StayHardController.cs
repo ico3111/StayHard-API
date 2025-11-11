12c28f5e82b4af80b70626e29a53dc0dfd81bb5f
ico <enricodparolin@gmail.com>
2025-11-10 21:38:48 -0300

    Adicionar arquivos de projeto.

---
+++ StayHard/Controllers/StayHardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StayHard.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StayHardController : ControllerBase
+    {
+        private readonly StayHard
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
